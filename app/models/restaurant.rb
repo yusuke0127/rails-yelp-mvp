b661a6819b81151a0f558802fbb61ae5ce5d0019
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-13 12:54:57 +0900

    Seeded the db

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ['chinese', 'japanese', 'italian', 'french', 'belgian']
+
   # associations
   has_many :reviews, dependent: :destroy
   #validations
   validates :name, :address, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: ['chinese', 'japanese', 'italian', 'french', 'belgian'] }
+  validates :category, inclusion: { in: CATEGORY }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CATEGORY = ['chinese', 'japanese', 'italian', 'french', 'belgian']
+
+
+puts "Creating new restaurants"
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORY.sample
+  )
+end
+
+puts "Done creating #{Restaurant.count} restaurants"
